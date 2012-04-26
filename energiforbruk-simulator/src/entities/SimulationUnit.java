8a3077960466dc1711b9a53f58ff8231875e237b
timkinmail@gmail.com <timkinmail@gmail.com@51254707-4f2e-b2c3-3907-c43b382ac744>
2012-04-26 10:18:24 +0000

    ridding av kode

--- energiforbruk-simulator/src/entities/SimulationUnit.java
+++ energiforbruk-simulator/src/entities/SimulationUnit.java
@@ -6,13 +6,34 @@ import java.util.ArrayList;
  *
  * @author 490501
  */
-public class SimulationUnit implements Runnable{
+public class SimulationUnit {
     //list of power consumers in an unit
     private ArrayList<PowerConsumer> consumers;
+    private double area; // boligs areal for eksempel
+    private int type;
 
-    @Override
-    public void run() {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public double getArea() {
+        return area;
     }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+    
+    @Override
+    public String toString() {
+        String a;
+        a = String.valueOf(area * type);
+        return a;
+    }
+
     
 }
