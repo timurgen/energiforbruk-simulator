eb296bb4c870ad6e62fe8360a2502ea52b94b13a
timkinmail@gmail.com <timkinmail@gmail.com@51254707-4f2e-b2c3-3907-c43b382ac744>
2012-05-04 09:43:55 +0000

    work in progress

--- trunk/SimulatorWeb/src/java/Tester.java
+++ trunk/SimulatorWeb/src/java/Tester.java
@@ -9,30 +9,30 @@ import exceptions.OversizedException;
  */
 public class Tester {
     public static void main(String[] args) throws OversizedException {
-        SimulationUnit sim1 = new SimulationUnit();
-        //walls
-        Surface wall_1 = new Surface(7000, 3050, 0.23);
-        Surface wall_2 = new Surface(3000,3050,0.23);
-        wall_2.addWindow(1500, 1000, 1.7);
-        Surface wall_3 = new Surface(7000,3050,0.23);
-        wall_3.addWindow(3500, 1000, 1.7);
-        Surface wall_4 = new Surface(3000,3050,0.23);
-        Surface floor = new Surface(7000,3000,0.17);
-        Surface roof = new Surface (7000,3000,0.15);
-        sim1.addWall(wall_1);
-        sim1.addWall(wall_2);
-        sim1.addWall(wall_3);
-        sim1.addWall(wall_4);
-        sim1.addWall(roof);
-        sim1.addFloor(floor);
-        double loss = sim1.computeHeatLoss(22, -15);
-        //System.out.println(loss);
-        double[] a = {28D,28D,28D,28D,28D,28D,30D,30D,31D,27D,27D};
-        sim1.setTempDiff(a);
-        Double[] b = sim1.computeConsumption(a.length);
-        for(Double c : b){
-        System.out.println(c);
-    }
+//        SimulationUnit sim1 = new SimulationUnit();
+//        //walls
+//        Surface wall_1 = new Surface(7000, 3050, 0.23);
+//        Surface wall_2 = new Surface(3000,3050,0.23);
+//        wall_2.addWindow(1500, 1000, 1.7);
+//        Surface wall_3 = new Surface(7000,3050,0.23);
+//        wall_3.addWindow(3500, 1000, 1.7);
+//        Surface wall_4 = new Surface(3000,3050,0.23);
+//        Surface floor = new Surface(7000,3000,0.17);
+//        Surface roof = new Surface (7000,3000,0.15);
+//        sim1.addWall(wall_1);
+//        sim1.addWall(wall_2);
+//        sim1.addWall(wall_3);
+//        sim1.addWall(wall_4);
+//        sim1.addWall(roof);
+//        sim1.addFloor(floor);
+//        double loss = sim1.computeHeatLoss(22, -15);
+//        //System.out.println(loss);
+//        double[] a = {28D,28D,28D,28D,28D,28D,30D,30D,31D,27D,27D};
+//        sim1.setTempDiff(a);
+//        Double[] b = sim1.computeConsumption(a.length);
+//        for(Double c : b){
+//        System.out.println(c);
+//    }
 
         
     }
