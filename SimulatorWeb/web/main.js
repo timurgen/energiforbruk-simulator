b2665f90cb9c407ee6f0514610d9255c9c294cc0
timkinmail@gmail.com <timkinmail@gmail.com@51254707-4f2e-b2c3-3907-c43b382ac744>
2012-05-29 13:25:24 +0000

    work in progress

--- SimulatorWeb/web/main.js
+++ SimulatorWeb/web/main.js
@@ -4,6 +4,7 @@
  */
 
 var counter = 1;
+var windowCounter = 1;
 /**
  * add new surface to form
  */
@@ -15,13 +16,30 @@ function addSurface() {
     newElement.innerHTML = '<h3>Construction # ' + (counter) + '</h3><br>'  ;
     newElement.innerHTML += 'Dimensions in mm: <input type=\"text\" name=\"length[]\" size=10 /> x <input type=\"text\" name=\"width[]\" size=10 /> ';
     newElement.innerHTML += 'U-value: <input type=\"text\" name=\"uvalues[]\" size=4 />';
+    newElement.innerHTML += '<button onclick=\"addWindow(\'Element'+counter+'\')\" type=\"button\">add window or door </button>';
     newElement.innerHTML += '<button onclick=\"removeSurface(\'Element'+counter+'\')\" type=\"button\">remove </button>';
     form.appendChild(newElement);
     counter++;
 }
 
+/**
+ * remove surface from form
+ */
 function removeSurface(id) {
 
     var form = document.getElementById("unitFormID");
     form.removeChild(document.getElementById(id));
+}
+
+function addWindow(id) {
+    var surface = document.getElementById(id);
+    var newElement = document.createElement('div');
+    newElement.setAttribute('class','windowForm')
+    newElement.setAttribute('id', 'Element'+windowCounter);
+    newElement.innerHTML = '<h3>Element of construction # ' + (windowCounter) + '</h3><br>'  ;
+    newElement.innerHTML += 'Dimensions in mm: <input type=\"text\" name=\"length[]\" size=10 /> x <input type=\"text\" name=\"width[]\" size=10 /> ';
+    newElement.innerHTML += 'U-value: <input type=\"text\" name=\"uvalues[]\" size=4 />';
+    newElement.innerHTML += '<button onclick=\"removeSurface(\'Element'+windowCounter+'\')\" type=\"button\">remove </button>';
+    surface.appendChild(newElement);
+    windowCounter++;
 }
