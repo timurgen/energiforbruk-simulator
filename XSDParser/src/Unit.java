a2125624630d8e63c7a538b214895fc3b36755c6
timkinmail@gmail.com <timkinmail@gmail.com@51254707-4f2e-b2c3-3907-c43b382ac744>
2012-08-30 20:29:17 +0000

    Parser for simulator based on XSD

---
+++ XSDParser/src/Unit.java
@@ -0,0 +1,18 @@
+
+import javax.xml.bind.annotation.XmlElement;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 490501
+ */
+public class Unit {
+    
+    @XmlElement
+    String projectName;
+    
+}
